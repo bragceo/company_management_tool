4974604c1e5cfe0e7d12be90a1304e61e0357687
bragceo <119948453+bragceo@users.noreply.github.com>
2023-04-30 16:52:00 -0400

    ​​set up the src/prompts.js file with Inquirer prompts

---
+++ src/prompts.js
@@ -0,0 +1,25 @@
+const inquirer = require('inquirer');
+
+async function mainMenu() {
+  return inquirer.prompt([
+    {
+      type: 'list',
+      name: 'action',
+      message: 'What would you like to do?',
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role',
+        'Exit'
+      ]
+    }
+  ]);
+}
+
+// Other prompts for adding and updating data go here
+
+module.exports = { mainMenu };
